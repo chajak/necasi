62b6393ea2544c9dd786d68705d449f7acceaaa0
Chajak <karel.necas@dmpublishig.cz>
2017-09-24 23:25:54 +0200

    JS updates

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,9 +3,16 @@ var locator = new function () {
     this.lng = null;
     this.address = null;
     this.search = null;
+    this.cookieObj = null;
 
     this.init = function () {
-        locator.tryGeolocation();
+        locator.cookieObj = cook.areCookiesSet();
+        if (!locator.cookieObj) {
+            locator.tryGeolocation();
+        }
+        else {
+            locator.setDataFromCookieObj();
+        }
     }
 
     this.tryGeolocation = function () {
@@ -13,7 +20,7 @@ var locator = new function () {
             navigator.geolocation.getCurrentPosition(locator.browserGeolocationSuccess, locator.browserGeolocationFail, { maximumAge: 50000, timeout: 20000, enableHighAccuracy: true });
         }
         else {
-            locator = this.tryAPIGeolocation();
+            locator = locator.tryAPIGeolocation();
         }
     };
 
@@ -75,7 +82,7 @@ var locator = new function () {
                 var response = "";
                 if (!!request.responseText) {
                     response = JSON.parse(request.responseText);
-                    if(!!response.results) {
+                    if (!!response.results) {
                         locator.updateSearch(response.results[0], true);
                     }
                 }
@@ -83,8 +90,8 @@ var locator = new function () {
         }
     }
 
-    this.getGpsFromAddress = function(searchedAddress, updateLocatorSearch) {
-        if(updateLocatorSearch == true) {
+    this.getGpsFromAddress = function (searchedAddress, updateLocatorSearch) {
+        if (updateLocatorSearch == true) {
             locator.search = searchedAddress;
         }
 
@@ -98,7 +105,7 @@ var locator = new function () {
                 var response = "";
                 if (!!request.responseText) {
                     response = JSON.parse(request.responseText);
-                    if(!!response.results) {
+                    if (!!response.results) {
                         locator.updateSearch(response.results[0], false);
                     }
                 }
@@ -106,16 +113,22 @@ var locator = new function () {
         }
     }
 
+    this.setCookieObj = function() {
+        locator.cookieObj = { lat: locator.lat, lng: locator.lng, address: JSON.stringify(locator.address), search: locator.search };
+        cook.setCookies(locator.cookieObj);
+    }
+
     this.updateSearch = function (address, updateSearchText) {
         locator.address = address;
         locator.lat = locator.address.geometry.location.lat;
         locator.lng = locator.address.geometry.location.lng;
-        
-        if(updateSearchText == true) {
+
+        if (updateSearchText == true) {
             locator.search = locator.address.formatted_address;
         }
 
         locator.updateFields();
+        locator.setCookieObj();
     }
 
     this.updateFields = function () {
@@ -123,13 +136,73 @@ var locator = new function () {
         document.getElementById("lat").value = locator.lat;
         document.getElementById("lng").value = locator.lng;
     }
+
+    this.setDataFromCookieObj = function (cookieObj) {
+        if(!!cookieObj) {
+            locator.cookieObj = cookieObj;
+        }
+
+        locator.lat = locator.cookieObj.lat;
+        locator.lng = locator.cookieObj.lng;
+        locator.address = locator.cookieObj.address;
+        locator.search = locator.cookieObj.search;
+
+        locator.updateFields();
+    }
 }
 
-locator.init();
+var cook = new function() {
+    this.setCookie = function(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+    
+    this.setCookies = function(cookieObj) {
+        var cookieDaysExpiration = 30;
+        for (var key in cookieObj) {
+            cook.setCookie(key, cookieObj[key], cookieDaysExpiration);
+        }
+    }
+    
+    this.getCookie = function(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    
+    this.areCookiesSet = function() {
+        var lat = cook.getCookie("lat");
+        if(!!lat) {
+            var lng = cook.getCookie("lng");
+            if(!!lng) {
+                var address = cook.getCookie("address");
+                if(!!address) {
+                    var search = cook.getCookie("search");
+                    if(!!search) {
+                        return { lat: lat, lng: lng, address: JSON.parse(address), search: search};
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+}
 
 function processForm(e) {
     var searchedAddress = document.getElementById("search").value;
-    if(searchedAddress == locator.search) {
+    if (searchedAddress == locator.search) {
         //not changed address
         console.log("Address: " + searchedAddress + " SAME (GPS: " + locator.lat + "," + locator.lng + ")");
     }
@@ -142,6 +215,7 @@ function processForm(e) {
 }
 
 window.onload = function () {
+    locator.init();
     var form = document.getElementById('searchForm');
     form.addEventListener("submit", processForm);
 };
